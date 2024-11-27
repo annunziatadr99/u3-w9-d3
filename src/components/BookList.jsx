ac24468d22346f55f9d1e0d2f091caf05da9fe77
annunziatadr99 <anudar99@hotmail.it>
2024-11-28 00:20:45 +0100

    fine proggetto

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,37 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, FormControl } from "react-bootstrap";
 import SigleBook from "./SingleBook";
+import { Component } from "react";
 
-const BookList = (props) => (
-  <Container>
-    <Row>
-      {props.books.map((book) => (
-        <Col xs={12} sm={6} md={4} lg={3} key={book.asin}>
-          <SigleBook book={book} />
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+class BookList extends Component {
+  state = {
+    SearchFilter: "", // abbiamo definito il parametro di  uno stato di partenza che in questo caso attende una stringa come valore del parametro
+  };
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={8}>
+            <FormControl
+              type="text"
+              value={this.state.SearchFilter} // il VALUE sarà uguale a SearchFilter: e.target.value
+              onChange={(e) => this.setState({ SearchFilter: e.target.value })}
+              placeholder="Search"
+            />
+          </Col>
+        </Row>
+        <Row xs={1} sm={2} md={4} lg={6}>
+          {this.props.books
+            .filter((book) =>
+              book.title
+                .toLowerCase()
+                .includes(this.state.SearchFilter.toLowerCase())
+            )
+            .map((book) => (
+              <SigleBook book={book} key={book.asin} />
+            ))}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default BookList;
